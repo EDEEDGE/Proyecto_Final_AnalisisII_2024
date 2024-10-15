057d7f6a0688d7901c7460ee24d396fd448f9e23
Elder Caal <panqueto55@gmail.com>
2024-10-14 22:49:06 -0600

    Archivo modificado

--- middlewares/autenticacion.js
+++ middlewares/autenticacion.js
@@ -2,21 +2,18 @@ const jwt = require('jsonwebtoken');// dependencia necesaria  para el JWT
 
 //middleware para verificar el token JWT
 const verificarToken = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1]; //encabezado para la autenticacion
+    const token = req.headers['authorization']?.split(' ')[1]; // Encabezado para la autenticación
 
-    //comprobamos si el token existe o si fue proporcionado
-    //lo manda a un json para ser leido por el navegador...
     if (!token) {
-        return res.status(403).send('Token no proporcionado. Se requiere el Token para acceder al Dashboard');
+        return res.status(403).json({ mensaje: 'Token no proporcionado. Se requiere el Token para acceder al Dashboard.' });
     }
 
     try {
-        const decode = jwt.verify(token,'clave_secreta_jwt');
-        req.user = decode;
-        next();
-    }
-    catch (error) {
-        return res.status(401).json({ mensaje: 'Token inválido o expirado' });
+        const decode = jwt.verify(token, 'clave_secreta_jwt'); // Verificamos el token
+        req.user = decode; // Almacenamos la información del usuario en la solicitud
+        next(); // Llamamos al siguiente middleware o controlador
+    } catch (error) {
+        return res.status(401).json({ mensaje: 'Token inválido o expirado.' }); // Respuesta si el token es inválido
     }
 };
 
