a7fecc5ffcd6465398d57a4565cdf377b37581b3
Elder Caal <panqueto55@gmail.com>
2024-10-14 18:59:00 -0600

    Creacion de registro y login de usuario :boom:

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 const express = require('express');//llamamos a express para crear el servidor
 const dbclient = require('./config/db');//llamamos al archivo db.js de la conexion de la base de datos
 const Cliente = require('./models/cliente')// llamamos a los modelos
+const usuarios = require('./routes/login')//llamar las rutas de login y registrar
+const cors = require('cors');//llamar cors para manejar peticiones dsede el frontend
+
 
 const app = express();
 const PORT = process.env.PORT || 3002;
+
+//habilitar cors para permitir peticiones desde el frontend(localhost3005)
+app.use(cors({
+    origin: 'http://localhost:3005' //direccion del otro frontend
+}));
 
 //Creamos un middleware para manejar el JSON
 app.use(express.json());
@@ -12,6 +20,8 @@ app.use(express.json());
 app.get('/', (req, res) =>{
     res.send('¡Bienvenido!, la aplicación esta funcionando correctamente... ');
 });
+
+app.use('/api/usuarios', usuarios);
 
 //sincronizar los modelos con la base de datos
 dbclient.sync().then(()=>{
