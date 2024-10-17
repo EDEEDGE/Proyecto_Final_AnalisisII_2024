071bcdae11195d4829e9bf4169cac70071268132
Elviis26 <elviismoralez@gmail.com>
2024-10-17 11:18:00 -0600

    Resolved conflicts after stash pop

--- config/db.js
+++ config/db.js
@@ -2,13 +2,13 @@
 require('dotenv').config();//cargar las variables de entorno desde el .env
 const {Sequelize} = require('sequelize');//llamamos a la dependencia sequelize para base de datos relacional
 
-const {DB_TYPE,DB_HOST,DB_PORT,BD_USER,DB_PASSWORD,DB_NAME} = process.env;
+const {DB_TYPE,DB_HOST,DB_PORT,DB_USER,DB_PASSWORD,DB_NAME} = process.env;
 
 let dbclient; //variable que controla que tipo de base de datos puede soportar el sistema
 
 switch(DB_TYPE){
     case 'postgresql':
-        dbclient = new Sequelize(DB_NAME,BD_USER,DB_PASSWORD,{
+        dbclient = new Sequelize(DB_NAME,DB_USER,DB_PASSWORD,{
             host:DB_HOST,
             port: DB_PORT,
             dialect:'postgres',
