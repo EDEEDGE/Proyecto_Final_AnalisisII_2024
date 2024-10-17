3c69164e5886175c839968875a5706973a1ce917
Elder Caal <panqueto55@gmail.com>
2024-10-16 23:39:56 -0600

    creacion de modelos reales para la base de datos. Creacion de controladores, rutas y relaciones de prueba :bug:

--- models/cliente.js
+++ models/cliente.js
@@ -1,16 +1,23 @@
-//archivo que se encargar de tener los modelos de la abse de datos
-const {DataTypes} = require('sequelize'); //llamamos a la dependencia para bases de datos relacional y le asignamos el nombre datatypes
-const dbclient=require('../config/db');//importar la conexion a la base de datos
+// archivo: models/cliente.js
+const { DataTypes } = require('sequelize');
+const dbclient = require('../config/db');
 
 const Cliente = dbclient.define('Cliente', {
     nombre: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
+    direccion: {
+        type: DataTypes.STRING,
+    },
+    correoElectronico: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    telefono: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
 });
 
--- models/usuario.js
+++ models/usuario.js
@@ -1,19 +1,16 @@
+// archivo: models/usuario.js
 const { DataTypes } = require('sequelize');
 const dbclient = require('../config/db');
 
-const Usuario = dbclient.define('Usuario',{
-    nombre:{
+const Usuario = dbclient.define('Usuario', {
+    nombre: {
         type: DataTypes.STRING,
         allowNull: false,
     },
-    credenciales:{
+    credenciales: {
         type: DataTypes.STRING,
         allowNull: false,
     },
 });
-
-// Relación: Un usuario tiene un empleado asociado
-Usuario.hasOne(Empleado, { foreignKey: 'idUsuario' }); // Un usuario tiene un empleado
-Empleado.belongsTo(Usuario, { foreignKey: 'idUsuario' }); // Un empleado pertenece a un usuario
 
 module.exports = Usuario;
