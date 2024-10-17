b144cb3777afb877365a9fd4d7e0e097d75173aa
Elder Caal <panqueto55@gmail.com>
2024-10-16 23:39:56 -0600

    creacion de modelos reales para la base de datos. Creacion de controladores, rutas y relaciones de prueba :bug:

--- models/producto.js
+++ models/producto.js
@@ -1,7 +1,7 @@
+// archivo: models/producto.js
 const { DataTypes } = require('sequelize');
 const dbclient = require('../config/db');
 
-//definir el modelo del producto
 const Producto = dbclient.define('Producto', {
     nombre: {
         type: DataTypes.STRING,
