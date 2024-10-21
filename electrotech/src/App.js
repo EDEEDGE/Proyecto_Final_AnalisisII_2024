da5f4d5a71567814b787f4471526a1d757bbecc2
by SeCo <selvinptiul@outlook.es>
2024-10-21 00:42:44 -0600

    :pushpin: Modificación de App.js para los nuevos diseños

---
+++ electrotech/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Bienvenido from './Bienvenido';
+//import Login from './Login'; 
+//import ControlPanel from './components/ControlPanel';
+
+function App() {
+    return (
+        <div className="App">
+            <Bienvenido />
+            {/*<Login />*/}
+            {/*<ControlPanel></ControlPanel>*/}
+        </div>
+    );
+}
+
+export default App;
