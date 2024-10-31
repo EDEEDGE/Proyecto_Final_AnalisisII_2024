94618edba873d1853cdcfa634ec85c797604d891
by SeCo <selvinptiul@outlook.es>
2024-10-31 00:33:52 -0600

    :bug: Solucion de errores y empeando la fusión del backend y el frontend

--- services/jwt.js
+++ services/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generarToken = (payload) =>{
-    return jwt.sign(payload, 'clave_secreta_jwt',{expiresIn : '10m'}); //no olvidar cambiar la duracion del token
+    return jwt.sign(payload, 'clave_secreta_jwt',{expiresIn : '90m'}); //no olvidar cambiar la duracion del token
 };
 
 const verificarToken = (token) =>{
